3bd599887b40c5997593820a367a39a0538a22f0
vladzur <vladzur@gmail.com>
2015-12-10 23:26:54 -0300

    Posts migration

---
+++ database/migrations/2015_12_11_020227_create_table_posts.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->dateTime('published_at');
+            $table->boolean('draft');
+            $table->string('slug');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
