6fc5a8953fdfca2c3620422ee47fa9045cdf95c7
vladzur <vladzur@gmail.com>
2015-12-12 16:20:13 -0300

    Assets and layouts

--- config/app.php
+++ config/app.php
@@ -145,6 +145,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Collective HTML
+         */
+        Collective\Html\HtmlServiceProvider::class
+
     ],
 
     /*
@@ -192,6 +197,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
 
     ],
 
--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,31 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss');
 });
+
+elixir(function(mix) {
+    mix.scripts(
+        [
+            'jquery/dist/jquery.min.js',
+            'bootstrap/dist/js/bootstrap.min.js'
+        ],
+        'public/js/app.js',
+        'node_modules'
+    )
+        .styles(
+            [
+                'admin.css'
+            ],
+            'public/css/admin.css'
+        )
+        .styles(
+            [
+                'blog.css'
+            ],
+            'public/css/blog.css'
+        ).styles(
+        [
+            'login.css'
+        ],
+        'public/css/login.css'
+    )
+});
